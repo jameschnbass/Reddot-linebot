0091bd3eecee78b3d9181e2f77b8b4d210551689
jameschnbass <jameschn.bass@gmail.com>
2020-03-01 01:20:38 +0800

    利用WEBAPI 更改MAC時 訂閱和取消訂閱TOPIC

--- routes/webapi.js
+++ routes/webapi.js
@@ -1,12 +1,19 @@
 var express = require('express');
 var router = express.Router();
 const redisclient = require('../lib/redisClient');
-
+const mqttclient = require('../lib/mqttClient');
 /* GET users listing. */
 router.post('/', function (req, res, next) {
 
+    redisclient.get('DeviceMAC',(error, MAC)=>{
+        mqttclient.unsubscribe('Advantech/' + MAC + '/data', () => {
+        });
+    });
+
     redisclient.set('DeviceMAC', req.body.MAC, () => {
         res.send('DeviceMAC :' + req.body.MAC);
+        mqttclient.unsubscribe('Advantech/' + req.body.MAC + '/data', () => {
+        });
     });
 
 });
