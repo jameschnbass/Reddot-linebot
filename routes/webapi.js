aebac4ad014be2bb1f1cb10180dcdc1b1797b392
jamesbass <jameschn.bass@gmail.com>
2020-05-25 20:26:54 +0800

    測試改動webapi

--- routes/webapi.js
+++ routes/webapi.js
@@ -4,26 +4,27 @@ const redisclient = require('../lib/redisClient');
 const mqttclient = require('../lib/mqttClient');
 /* GET users listing. */
 router.post('/DeviceMAC', function (req, res, next) {
-    redisclient.hgetall('DeviceMAC', function (err, allDeviceMAC) {
-        mqttclient.unsubscribe('Advantech/' + allDeviceMAC + '/data', () => {});
+    mqttclient.sadd('DeviceMAC', req.body.MAC, function (err, res) {
+        if (err) throw err;
         redisclient.set('DeviceMAC', req.body.MAC, () => {
             redisclient.hset('Advantech/' + req.body.MAC + '/data', 'light', 'red');
             redisclient.hset('Advantech/' + req.body.MAC + '/data', 'online', 'false');
             res.send('DeviceMAC :' + req.body.MAC + 'is Added.');
-            mqttclient.subscribe('Advantech/' + req.body.MAC + '/data', () => {});
         });
     });
 });
 
 router.get('/DeviceMAC', function (req, res, next) {
-    //取得DeviceMAC
-    res.send('DeviceMAC :get.');
-
+    redisclient.smembers('DeviceMAC', function (err, members) {
+        if (err) throw err;
+        //取得DeviceMAC
+        res.send(members);
+        console.log(members);
+    });
 });
 
 router.delete('/DeviceMAC', function (req, res, next) {
     //刪除
-
     res.send('DeviceMAC :delete.');
 });
 
