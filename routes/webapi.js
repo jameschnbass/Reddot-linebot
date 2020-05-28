7a666987dab88584244873eac4a9843a9a0765b8
james <jameschn.bass@gmail.com>
2020-05-28 12:38:41 +0800

    修正一些問題

--- routes/webapi.js
+++ routes/webapi.js
@@ -4,20 +4,25 @@ const redisclient = require('../lib/redisClient');
 const mqttclient = require('../lib/mqttClient');
 /* GET users listing. */
 
-router.post('/DeviceMAC', function (req, res, next) {
+router.post('/DeviceMAC', function (req, response, next) {
     redisclient.sadd('DeviceMAC', req.body.MAC, function (err, res) {
-        if (err) throw err;
-        redisclient.set('DeviceMAC', req.body.MAC, () => {
+        if (err) {
+            console.log(err);
+            return;
+        };
             redisclient.hset('Advantech/' + req.body.MAC + '/data', 'light', 'red');
             redisclient.hset('Advantech/' + req.body.MAC + '/data', 'online', 'false');
-            res.send('DeviceMAC :' + req.body.MAC + 'is Added.');
-        });
+            response.send('DeviceMAC :' + req.body.MAC + 'is Added.');
+       
     });
 });
 
 router.get('/DeviceMAC', function (req, res, next) {
     redisclient.smembers('DeviceMAC', function (err, members) {
-        if (err) throw err;
+        if (err) {
+            console.log(err);
+            return;
+        };
         //取得DeviceMAC
         res.send(members);
         console.log(members);
